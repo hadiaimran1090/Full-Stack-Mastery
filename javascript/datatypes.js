6aaf9d81bac10822ec3038188f2133680897bc0c
Hadia Imran <hadiaimran730@gmail.com>
2025-06-12 17:25:54 +0500

    commit

---
+++ javascript/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer vesion of javascript
+
+//alert(3+3)// we are using nodejs,not browser, so no need to use alert
+
+console.log(3+3)
+console.log("hello")
+
+let name = "hadia imran";
+let age =19;
+let isStudent = true;
+let state = null; 
+let notAssigned; //undefined
+
+//null => empty value (standalone value) type of null is object
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true or false
+//undefined => variable is declared but not assigned any value
+//symbol => unique value
+
+//object
+
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof isStudent) //boolean
+console.log(typeof state)  //type of null is object
+console.log(typeof notAssigned) //undefined
