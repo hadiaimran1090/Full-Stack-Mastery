fc5a57418558687c0b934a9d0e0f74eca4360565
Hadia Imran <hadiaimran730@gmail.com>
2025-06-17 18:37:04 +0500

    Explanation of stack call and global execution context

---
+++ javascript/callstack.js
@@ -0,0 +1,13 @@
+//1.Global Eecution context is different for different environment
+     //for browser  value of this  object is window
+      // js code is run in global execution context
+    //1*.Memory phase(creation phase)   
+       //memory allocation to all variables
+        //function definition
+    //2*.Execution phase
+       //Variables get real values.
+       // Functions execute when called 
+       // eg When sayHello() runs, a new execution context(Memory phase+execution phase) is created inside the global one.
+//2.function execution context
+//3.Eval excution context
+
