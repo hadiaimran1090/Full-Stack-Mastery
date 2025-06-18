1dbbba0f12de4d388850b4bf960273ac56fc4d86
Hadia Imran <hadiaimran730@gmail.com>
2025-06-18 15:54:29 +0500

    While and do while loop

---
+++ javascript/iterations.js
@@ -0,0 +1,19 @@
+//while loop
+let index=0
+while(index<10){
+    console.log(`value of index is ${index}`)
+    index++;
+}
+let arr=["hadia","anika","mahum"]
+let arr1=0;
+while(arr1<arr.length){
+    console.log(arr[arr1])
+    arr1++;
+}
+
+// do while loop
+let score=0;
+do{
+  console.log(` score is ${score}`)
+   score++;
+}while(score<10)
