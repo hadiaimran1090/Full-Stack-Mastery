ad381e7c4418e7fc979e06917c1762bf645dd91d
Hadia Imran <hadiaimran730@gmail.com>
2025-06-22 15:13:02 +0500

    sync and async javascript

---
+++ javascript/async.js
@@ -0,0 +1,14 @@
+/**********javascript************(default)
+ synchronus
+ single threaded
+
+ */
+/*************EXECUTION CONTEXT 
+  single line  of code excute at a time
+  every operation waits for last operation to be complete
+*/
+
+/* Blocking(read file sync)(block flow of program)
+ vs
+ non blocking code(read file async)(donot block execution)
+ */
